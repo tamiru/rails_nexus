1cd929a4aca0ff6ae32cdb1f8861f50bcc173a13
Tamiru Hailu <tamiruhailu@gmail.com>
2026-08-21 22:16:40 +0300

    ransack fix

--- lib/faultline/ransack_config.rb
+++ lib/faultline/ransack_config.rb
@@ -1,26 +1,4 @@
 # frozen_string_literal: true
 
-# Configure Ransack for Faultline LoggedException model.
-# Ransack 4.x automatically adds `.ransack` to ActiveRecord::Relation via its railtie.
-# We just need to declare which attributes are searchable.
-module Faultline
-  module RansackConfig
-    def self.apply!
-      return unless defined?(Ransack)
-
-      Faultline::LoggedException.class_eval do
-        def self.ransackable_attributes(auth_object = nil)
-          %w[action_name backtrace controller_name created_at environment
-             exception_class id message remote_ip request
-             updated_at user_agent user_info]
-        end
-
-        def self.ransackable_associations(auth_object = nil)
-          []
-        end
-      end
-    end
-  end
-end
-
-Faultline::RansackConfig.apply!
+# Ransack configuration is now defined directly on Faultline::LoggedException
+# in app/models/faultline/logged_exception.rb.
